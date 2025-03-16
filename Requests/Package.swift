06f8cfde86dd13fc7ecdfd5ddc7615e07be4b551
e-kononenko <>
2025-03-16 15:03:02 +0100

    Created Package

---
+++ Requests/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Requests",
+    platforms: [
+        .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Requests",
+            targets: ["Requests"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Requests"),
+        .testTarget(
+            name: "RequestsTests",
+            dependencies: ["Requests"]
+        ),
+    ]
+)
---
+++ Requests/Sources/Requests/Requests.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public enum RequetsTest {
+    public func test() {
+        print("Testing package Requests")
+    }
+}
+
