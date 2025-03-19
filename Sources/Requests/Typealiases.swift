ab0df7bf933044627563d1cdcce71230cbd72022
e-kononenko <>
2025-03-19 20:11:35 +0100

    Adjusted typealiases

--- Sources/Requests/Typealiases.swift
+++ Sources/Requests/Typealiases.swift
@@ -5,5 +5,5 @@
 //  Created by Evgenii Kononenko on 19.03.25.
 //
 
-public typealias JSONAPIRequest = APIRequest & JSONDataDecoding
+public typealias APIRequestJSON = APIRequest & JSONDataDecoding
 public typealias BundledFileRequestJSON = BundleFileRequest & JSONDataDecoding
