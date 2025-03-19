e259ba7c366d8059ee18847042c5a1fad70f2166
e-kononenko <>
2025-03-19 20:13:15 +0100

    fixed JSONDataDescoding

--- Sources/Requests/DataDecoding/JSONDataDecoding.swift
+++ Sources/Requests/DataDecoding/JSONDataDecoding.swift
@@ -11,8 +11,8 @@ public protocol JSONDataDecoding: DataDecoding {
     var decoder: JSONDecoder { get }
 }
 
-extension JSONDataDecoding where Output: Decodable {
-    public func decode(_ data: Data) throws -> Output {
+public extension JSONDataDecoding where Output: Decodable {
+    func decode(_ data: Data) throws -> Output {
         return try decoder
             .decode(Output.self, from: data)
     }
